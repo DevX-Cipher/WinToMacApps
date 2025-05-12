6d07b823fd3b0429b6ef6ed277a4292b4f3b9cb3
modz2014 <dawgz1992.jp@gmail.com>
2025-05-12 19:58:48 +0800

    update
    adjust toolchain

--- macosx-toolchain.cmake
+++ macosx-toolchain.cmake
@@ -1,17 +1,31 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Specify Clang compilers (using your exact installation path)
+# Use system environment variables for compiler paths
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
-# macOS SDK path
-set(MACOSX_SDK "C:/MacOSX10.15.sdk")
-set(MACOSX_DEPLOYMENT_TARGET "10.13")
+# Retrieve SDK path dynamically or set as an environment variable
+set(MACOSX_SDK $ENV{MACOSX_SDK})
+set(MACOSX_DEPLOYMENT_TARGET $ENV{MACOSX_DEPLOYMENT_TARGET})
+
+# Fallback in case environment variables are not set
+if(NOT MACOSX_SDK)
+    message(FATAL_ERROR "MACOSX_SDK environment variable is not set!")
+endif()
+
+if(NOT MACOSX_DEPLOYMENT_TARGET)
+    set(MACOSX_DEPLOYMENT_TARGET "10.13")  # Default value
+endif()
 
 # Compiler flags
-set(CMAKE_C_FLAGS "--target=x86_64-apple-darwin --sysroot=${MACOSX_SDK} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}")
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}")
+set(CMAKE_C_FLAGS "--target=x86_64-apple-darwin -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} --sysroot=${MACOSX_SDK}" CACHE STRING "C flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "C++ flags")
 
 # Linker flags
-set(CMAKE_EXE_LINKER_FLAGS "-L${MACOSX_SDK}/usr/lib")
+set(CMAKE_EXE_LINKER_FLAGS "-target x86_64-apple-darwin --sysroot=${MACOSX_SDK} -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} -L${MACOSX_SDK}/usr/lib -fuse-ld=lld" CACHE STRING "Linker flags")
+
+message(STATUS "Toolchain: C compiler = ${CMAKE_C_COMPILER}")
+message(STATUS "Toolchain: C++ compiler = ${CMAKE_CXX_COMPILER}")
+message(STATUS "Toolchain: SDK = ${MACOSX_SDK}")
+message(STATUS "Toolchain: Deployment Target = ${MACOSX_DEPLOYMENT_TARGET}")
